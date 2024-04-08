93c3340f87ddf1185e4f60505febdfc079d9105f
luminous <910667665@qq.com>
2024-04-08 22:05:52 +0800

    feat:下拉框，下滑到底部，会自动加载更多选项（请求分页接口）

---
+++ src/views/RolePage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div v-for="(role, index) in rolesArr" class="role-item" :key="role.id" >
+      <div>{{ role.name + (index + 1)}} </div>
+      <input type="text" placeholder="请输入内容">
+
+    </div>
+    <br/>
+    <button @click="addRole">Add</button>
+  </div>
+</template>
+
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      rolesArr: [
+        {
+          id: 1,
+          name: '规则'
+        },
+      ]
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+    addRole() {
+      this.rolesArr.push({
+        id: this.rolesArr.length + 1,
+        name: '规则'
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* .role-item {
+  display: flex;
+} */
+</style>
